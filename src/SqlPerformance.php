aaf0e818702c77b75cc79e7844a0d85dc354121b
Facundo Igua <facundoigua@gmail.com>
2024-05-24 17:43:32 -0300

    [MED-704] Fix migration PHP 8

--- src/SqlPerformance.php
+++ src/SqlPerformance.php
@@ -71,7 +71,7 @@ class SqlPerformance implements ISqlPerformance, JsonSerializable
      * which is a value of any type other than a resource.
      * @since 5.4.0
      */
-    public function jsonSerialize()
+    public function jsonSerialize(): array
     {
         return
             [
